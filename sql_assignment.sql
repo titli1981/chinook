8098df2625defd70a542e27753b40c0a4e9f0c69
titli1981 <madhumishra084@gmailcom>
2016-08-30 15:01:45 -0400

    plain sql

--- sql_assignment.sql
+++ sql_assignment.sql
@@ -5,19 +5,25 @@
 -- below each of the following questions:
 
 -- 1) Find the albums recorded by the artist Queen.
+		SELECT id FROM artists WHERE name='Queen';
+		SELECT * FROM albums WHERE artist_id=51;
 
 
-
--- 2) [Count](http://www.w3schools.com/sql/sql_func_count.asp) how many tracks belong to the media type "Protected MPEG-4 video file".
-
+-- 2) [Count](S) how many tracks belong to the media type "Protected MPEG-4 video file".
+   SELECT * FROM media_types;
+   \dt
+   
+   SELECT COUNT(*) FROM tracks WHERE media_type_id=3;
 
 
 -- 3) Find the least expensive track that has the genre "Electronica/Dance".
+   SELECT * FROM genres WHERE name='Electronica/Dance';
+   SELECT unit_price FROM tracks WHERE genre_id = 15;
+   SELECT * FROM tracks WHERE genre_id = 15 ORDER BY unit_price ASC LIMIT 1;
 
+-- 4) Find  all the artists whose names start with A.
 
-
--- 4) Find the all the artists whose names start with A.
-
-
+   SELECT name FROM artists WHERE name ILIKE 'A%';
 
 -- 5) Find all the tracks that belong to playlist 1.
+SELECT tracks.id, tracks.name FROM playlists_tracks, tracks WHERE playlist_id = 1 AND playlists_tracks.track_id=tracks.id;
